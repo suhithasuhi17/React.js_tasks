4d0a8123c9fdc1c3f12b94710dc420fcef6ba737
suhitha <157236546+suhithasuhi17@users.noreply.github.com>
2025-04-17 22:50:07 +0530

    Dashboard_page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+import DashBoard from './Components/DashBoard';
+function App() {
+  return (
+    <>
+    <DashBoard/> 
+      
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/DashBoard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div >
+            <div >
+                <h1  >
+                    USER NAME!
+                </h1>
+            </div>
+            <div >
+                <ul>
+                    <li >Teacher management</li>
+                    <li>Student management</li>
+                    <li>Attendance management</li>
+                    <li>Settings</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+ export  default Dashboard;
